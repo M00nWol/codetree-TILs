633d042acd4717c4b5fa8d0d8a6698340fd56e7d
M00nWol <129468327+M00nWol@users.noreply.github.com>
2024-09-23 21:28:25 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240923/다양하게 출력/print-in-variety.cpp
+++ 240923/다양하게 출력/print-in-variety.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-    cout << "Total days in Year" << endl << 365 <<endl << "Circumference rate" << endl << 3.1415926535;
+    double a = 3.1415926535;
+    cout << "Total days in Year" << endl << 365 <<endl << "Circumference rate" << endl;
+    cout.precision(11);
+    cout << a;
     return 0;
 }
