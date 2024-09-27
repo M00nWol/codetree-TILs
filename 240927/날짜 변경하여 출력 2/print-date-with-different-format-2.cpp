cc0282ce95c41413c36ed6a769804e8ede8f6bbd
M00nWol <129468327+M00nWol@users.noreply.github.com>
2024-09-27 13:03:18 +0900

    Title: 날짜 변경하여 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240927/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    char m, n;
+    
+    cint << a << m << b << n << c;
+    cout << c << "." << a << "." << b;
+    return 0;
+}
