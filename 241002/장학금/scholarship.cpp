18f8b234b10bf17168016d7cee2f0ba5ceb88549
M00nWol <129468327+M00nWol@users.noreply.github.com>
2024-10-02 00:19:02 +0900

    Title: 장학금, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241002/장학금/scholarship.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, m;
+    cin >> a >> b;
+
+    if(a >= 90){
+        if(b >= 95){
+            m = 100000;
+        }else if(b>=90){
+            m = 50000;
+        }else {
+            m = 0;
+        }
+    } else {
+        m = 0
+    }
+
+    cout << m;
+    return 0;
+}
