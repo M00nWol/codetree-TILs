e8fa0bb6281553310feba8777df4637258a3c588
M00nWol <129468327+M00nWol@users.noreply.github.com>
2024-10-02 00:44:26 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241002/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   char A, B, C;
+   int a, b, c, e;
+   e = 0;
+
+   cin >> A >> a >> B >> b >> C >> c;
+
+   if(A=='Y'&&a>=37){
+    if((B=='Y'&&b>=37) || (C=='Y'&&C>=37) )
+        cout << 'E';
+    else
+        cout << 'N';
+   }else if((B=='Y'&&b>=37) || (C=='Y'&&C>=37)){
+        cout << 'E';
+   }
+    else
+        cout << 'N';
+
+    return 0;
+}
