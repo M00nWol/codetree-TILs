b93405f8c23999e1a5f7379ea2d139887529b1d3
M00nWol <129468327+M00nWol@users.noreply.github.com>
2024-10-01 12:18:05 +0900

    Title: 합과 차의 나눗셈, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241001/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    sum = a+b;
+    sub = a-b;
+    cout.precision(2);
+    cout << double(sum)/sub;
+    return 0;
+}
