db96071ff9480e3a9d09c48ab3de4a5969d6e2f6
M00nWol <129468327+M00nWol@users.noreply.github.com>
2024-10-04 12:34:14 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241004/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int i;
+    if (a%2==0) i=a;
+    else i=a+1;
+
+    while (i<=b){
+        cout << i << " ";
+        i+=2;
+    }
+    return 0;
+}
