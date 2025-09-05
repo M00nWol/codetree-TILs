bcbe495415399c657088d6320ccadbfcece4a70f
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-09-05 16:43:08 +0900

    Title: 특정 수 맞히기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250905/특정 수 맞히기/catching-specific-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    while(n!=25){
+        if(n<25){
+            cout << "Higher" << endl;
+        } else {
+            cout << "Lower" << endl;
+        }
+        cin >> n;
+    }
+
+    cout << "Good";
+    return 0;
+}
