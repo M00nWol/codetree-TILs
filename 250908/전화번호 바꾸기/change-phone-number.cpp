fe741a0882533ca479a96f9376a34774326c060f
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-09-08 11:11:22 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250908/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int z, x, y;
+    char a, b;
+
+    cin >> z >> a >> x >> b >> y;
+
+    cout << "010" << a << y << b << x;
+    return 0;
+}
