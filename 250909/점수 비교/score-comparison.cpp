cdce551f6ab822818a866a09bdf8db25a28b5ac3
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-09-09 11:05:46 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250909/점수 비교/score-comparison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int ma, mb, ea, eb;
+
+    cin >> ma >> ea;
+    cin >> mb >> eb;
+
+    if((ma>mb)&&(ea>eb)){
+        cout << 1;
+    } else {
+        cout << 0;
+    }
+    return 0;
+}
