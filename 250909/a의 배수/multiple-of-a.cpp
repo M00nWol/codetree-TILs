c3e449dd7763cddcbf382249c789663c149d41a2
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-09-09 18:00:59 +0900

    Title: a의 배수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250909/a의 배수/multiple-of-a.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a;
+    int i = 1;
+
+    cin >> n >> a;
+
+    while(i<=n){
+        if(i%a == 0) {
+            cout << 1 << endl;
+        } else {
+            cout << 0 << endl;
+        }
+        i++;
+    }
+    return 0;
+}
