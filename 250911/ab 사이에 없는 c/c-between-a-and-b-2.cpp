6708ced345272b528d2f8b1ec359864d1c8f590b
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-09-11 10:50:14 +0900

    Title: ab 사이에 없는 c, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250911/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    bool res = true;
+
+    cin >> a >> b >> c;
+
+    for(int i=a; i<=b; i++){
+        if(i%c==0){
+            res = false;
+            break;
+        }
+    }
+
+    if(res){
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+    return 0;
+}
