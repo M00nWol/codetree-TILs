46e77851d9156c62694de8b5220dbd36d7ba4a26
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-09-11 10:19:45 +0900

    Title: 몫 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250911/몫 출력하기/print-share.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt=0;
+
+    while(cnt<3){
+        cin >> n;
+
+        if(n%2==0){
+            cout << n/2 << endl;
+            cnt++;
+        }
+    }
+    
+    return 0;
+}
