de3ba2b5a937d08d6ad6c50bb5fd0981d7b0e5a9
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-09-11 10:42:25 +0900

    Title: 조건에 따른 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250911/조건에 따른 연산/operatino-by-rule.cpp
+++ 250911/조건에 따른 연산/operatino-by-rule.cpp
@@ -8,12 +8,12 @@ int main() {
     cin >> n;
 
     while(n<1000){
+        cnt++;
         if(n%2==0){
             n = n*3+1;
         } else {
-            n = n*2+1;
+            n = n*2+2;
         }
-        cnt++;
     }
 
     cout << cnt;
