2f5e2467d5747538aad0c7f2b305ec8552a9bc0f
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-09-12 10:23:43 +0900

    Title: 별표 출력하기 10, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250912/별표 출력하기 10/print-star-10.cpp
+++ 250912/별표 출력하기 10/print-star-10.cpp
@@ -6,25 +6,12 @@ int main() {
 
     cin >> n;
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<2*n; i++){
         if(i%2==0){
-            for(int j=0; j<(i/2)+1; j++){
+            for(int j=0; j<(i/2)+1;j++){
                 cout << "* ";
             }
-        } else{
-            for(int j=0; j<n-(i/2); j++){
-                cout << "* ";
-            }
-        }
-        cout << endl;
-    }
-
-    for(int i=n-1; i>=0; i--){
-        if(i%2==0){
-            for(int j=0; j<(i/2)+1; j++){
-                cout << "* ";
-            }
-        } else{
+        } else {
             for(int j=0; j<n-(i/2); j++){
                 cout << "* ";
             }
