3edd458555fe0ee38d40f877c21a26d82b8a0857
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-09-15 11:25:04 +0900

    Title: 소수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250915/소수 구하기/get-prime.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    bool prime = true;
+
+    cin >> n;
+
+    for(int i=2; i<=n; i++){
+        prime = true;
+
+        for(int j=2; j<i; j++){
+            if(i%j==0){
+                prime = false;
+                break;
+            }
+        }
+
+        if(prime){
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
