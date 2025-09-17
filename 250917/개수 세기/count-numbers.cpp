41248618a9d5fa8f9dd6f77ce4906ca72eec2a49
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-09-17 09:55:51 +0900

    Title: 개수 세기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250917/개수 세기/count-numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    int arr[100];
+    int cnt = 0;
+
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<n; i++){
+        if(arr[i]==m){
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
