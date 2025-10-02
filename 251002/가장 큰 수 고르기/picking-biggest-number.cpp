3befb10073f92aea98143a55a7dd85f64ea23384
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-02 11:19:17 +0900

    Title: 가장 큰 수 고르기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251002/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    int max = 0;
+
+    for(int i=0; i<10; i++){
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<10; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+
+    cout << max << endl;
+    return 0;
+}
