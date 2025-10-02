c5784e6aac32d9f2ea9a352faa5800b98f86ec3a
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-02 14:55:10 +0900

    Title: 500 근처의 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251002/500 근처의 수/near-500.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int max, min;
+    int arr[10];
+
+    for(int i=0; i<10; i++){
+        cin >> arr[i];
+    }
+
+    max = 0;
+    min = 1000;
+    for(int i=0; i<10; i++){
+        if(arr[i]<500 && arr[i]>max){
+            max = arr[i];
+        }
+
+        if(arr[i]>500 && arr[i]<min){
+            min = arr[i];
+        }
+    }
+
+    cout << max << " " << min;
+    return 0;
+}
