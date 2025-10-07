7c5978cb36cf273ac04d4854910acc66ba0538e7
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-07 16:51:16 +0900

    Title: 두 배열의 곱, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251007/두 배열의 곱/multiple-of-two-arrays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[3][3], b[3][3], c[i][j];
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            cin >> b[i][j];
+        }
+    }
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            c[i][j] = a[i][j]*b[i][j];
+            cout << c[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
