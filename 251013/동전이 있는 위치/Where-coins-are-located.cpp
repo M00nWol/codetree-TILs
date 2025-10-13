761510ec19db52a1fa5df565843363e306ce896e
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-13 16:40:57 +0900

    Title: 동전이 있는 위치, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251013/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    int a, b;
+    int arr[9][9] = {};
+
+    cin >> n >> m;
+
+    for(int i=0; i<m; i++){
+        cin >> a >> b;
+        arr[a-1][b-1] = 1;
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
