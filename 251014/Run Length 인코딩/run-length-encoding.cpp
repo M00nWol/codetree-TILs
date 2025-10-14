a34513b7a0c3d9ddfce5fe0ae57b2c1d759dacb0
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-14 16:13:09 +0900

    Title: Run Length 인코딩, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251014/Run Length 인코딩/run-length-encoding.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string A;
+
+int main() {
+    cin >> A;
+    int cnt = 1;
+    int len = A.length();
+    char c[1000];
+    string s[1000];
+    int idx = 0;
+    int sum = 0;
+
+    for(int i=1; i<len; i++){
+        if(A[i] == A[i-1]){
+            cnt++;
+        } else{
+            c[idx] = A[i-1];
+            s[idx] = to_string(cnt);
+            sum += (1+s[idx].length());
+            idx++;
+            cnt = 1;
+        }
+
+        if(i==len-1){
+            c[idx] = A[i-1];
+            s[idx] = to_string(cnt);
+            sum += (1+s[idx].length());
+            idx++;
+        }
+    }
+
+    cout << sum << endl;
+    for(int i=0; i<idx; i++){
+        cout << c[i] << s[i];
+    }
+    return 0;
+}
