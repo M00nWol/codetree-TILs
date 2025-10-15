75e8a412612acc42a3d5f8f5f0662be66f4af97f
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-15 16:25:34 +0900

    Title: 규칙에 따라 밀기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251015/규칙에 따라 밀기/push-by-the-rules.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a;
+    string cmd;
+
+    cin >> a >> cmd;
+
+    int len = a.length();
+
+    for(int i=0; i<cmd.length(); i++){
+        if(cmd[i] == 'L'){
+            a = a.substr(1, len-1) + a.substr(0, 1);
+        } else {
+            a = a.substr(len-1, 1) + a.substr(0, len-1);
+        }
+    }
+
+    cout << a;
+    return 0;
+}
