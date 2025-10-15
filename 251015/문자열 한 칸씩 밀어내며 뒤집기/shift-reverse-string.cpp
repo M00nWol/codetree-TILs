f425f179d685347990548c9370a70b2f62c72b17
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-15 16:22:34 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 11ms, Memory: 1MB, Status: Passed - Codetree

--- 251015/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.cpp
+++ 251015/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.cpp
@@ -17,7 +17,7 @@ int main() {
         }else if(a==2){
             s = s.substr(len-1, 1) + s.substr(0, len-1);
         }else {
-            tmp = ""
+            tmp = "";
             for(int j=len-1; j>=0; j--){
                 tmp += s[j];
             }
