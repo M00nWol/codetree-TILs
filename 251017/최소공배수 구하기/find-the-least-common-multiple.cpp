5ff04f1b77122155460d0d4942ded1feb4a965bc
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-17 10:53:45 +0900

    Title: 최소공배수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251017/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 251017/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -6,16 +6,20 @@ int n, m;
 
 void lcm(int a, int b){
     int i = a;
+    int plus = a;
     if(b>a) {
         i = b;
+        plus = b;
     }
 
+
     while(true){
-        if(i%a==0&&i%b==0){
+        if((i%a==0)&&(i%b==0)){
             cout << i;
             break;
+        } else{
+            i += plus;
         }
-        i += i;
     }
 }
 
