b36a5bf63d0f547a014b9cd5deb31f66bf625bdc
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-22 14:34:06 +0900

    Title: Factorial, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251022/Factorial/factorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int fact(int n){
+    if(n==1){
+        return 1;
+    }
+
+    return fact(n-1)*n;
+}
+
+int main() {
+    cin >> N;
+
+    cout << fact(N);
+
+    return 0;
+}
