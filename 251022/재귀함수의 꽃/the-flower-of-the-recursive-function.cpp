17e073558319bc0b30b2c0c06385991e9cf9fc9e
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-22 12:45:58 +0900

    Title: 재귀함수의 꽃, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251022/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void Print(int n){
+    if(n==0){
+        return;
+    }
+
+    cout << n << " ";
+    Print(n-1);
+    cout << n << " "; 
+}
+
+int main() {
+    cin >> N;
+
+    Print(N);
+
+    return 0;
+}
