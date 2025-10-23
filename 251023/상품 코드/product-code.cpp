158a634490273b0c3b7828c7ba7cc4e1982e8a57
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-23 18:01:53 +0900

    Title: 상품 코드, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251023/상품 코드/product-code.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string product_name;
+int product_code;
+
+class Product{
+    public:
+        string name;
+        int code;
+
+        Product(string name, int code){
+            this->name = name;
+            this->code = code;
+        }
+};
+
+int main() {
+    cin >> product_name >> product_code;
+
+    Product p1 = Product("codetree", 50);
+    Product p2 = Product(product_name, product_code);
+
+    cout << "product " << p1.code << " is " << p1.name << endl;
+    cout << "product " << p2.code << " is " << p2.name;
+    return 0;
+}
