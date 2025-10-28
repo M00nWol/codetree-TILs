d690e19f997be9888455fb3c5be78f2afb632daa
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-28 15:49:01 +0900

    Title: 선두를 지켜라, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 251028/선두를 지켜라/keep-the-lead.cpp
+++ 251028/선두를 지켜라/keep-the-lead.cpp
@@ -9,7 +9,7 @@ int main() {
     int t_a = 1;
     int t_b = 1;
     int cnt = 0;
-    int prev = 0;
+
 
     cin >> n >> m;
 
@@ -33,6 +33,7 @@ int main() {
         c[i] = a[i]-b[i];
     }
 
+    int prev = c[0]
     for(int i=1; i<t_a; i++){
         if(c[i]==0){
             continue;
