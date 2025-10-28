56525a7d2f714fb6b0681b480e5f9766219b9d65
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-28 17:21:35 +0900

    Title: 좌우로 움직이는 로봇, Time: 17ms, Memory: 1MB, Status: Runtime Error - Codetree

---
+++ 251028/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    int a[50001]={}, b[50001]={};
+    int timea=1, timeb=1;
+    int time, t;
+    char d;
+    int cnt = 0;
+
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++){
+        cin >> t >> d;
+        if(d=='L'){
+            while(t--){
+                a[timea] = a[timea-1]-1;
+                timea++;
+            }
+        } else {
+            while(t--){
+                a[timea] = a[timea-1]+1;
+                timea++;
+            }
+        }
+    }
+
+    for(int i=0; i<m; i++){
+        cin >> t >> d;
+        if(d=='L'){
+            while(t--){
+                b[timeb] = b[timeb-1]-1;
+                timeb++;
+            }
+        } else {
+            while(t--){
+                b[timeb] = b[timeb-1]+1;
+                timeb++;
+            }
+        }
+    }
+    
+    for(int i=50000; i>=timea; i--){
+        a[i] = a[timea-1];
+    }
+
+    for(int i=50000; i>=timeb; i--){
+        b[i] = b[timeb-1];
+    }
+
+    
+    time = (timea>timeb) ? timea : timeb;
+
+
+    for(int i=1; i<time; i++){
+        if(a[i]==b[i] && a[i-1]!=b[i-1]){
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
