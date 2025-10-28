153349e715252ce93d35f3c633e610474ef32742
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-28 14:53:51 +0900

    Title: 벌금은 누구에게, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251028/벌금은 누구에게/who-will-pay.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, k, tmp;    
+
+    cin >> n >> m >> k;
+
+    int student[n+1] = {};
+
+    for(int i=0; i<m; i++){
+        cin >> tmp;
+        student[tmp]++;
+
+        if(student[tmp] >= k){
+            cout << tmp;
+            break;
+        }
+    }
+
+    return 0;
+}
