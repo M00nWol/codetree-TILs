1b8722e400b540cb39101bba76b35a5586799349
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-10-28 09:42:29 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251028/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+#define OFFSET 1000
+#define MAX_R 2000
+
+int main() {
+    int x1[2], y1[2], x2[2], y2[2];
+    int rec[MAX_R+1][MAX_R+1] = {};
+    int sum = 0;
+    int minx, miny, maxx, maxy;
+
+    for(int i=0; i<2; i++){
+        cin >> x1[i] >> y1[i] >> x2[i] >> y2[i];
+
+        x1[i]+=OFFSET;
+        y1[i]+=OFFSET;
+        x2[i]+=OFFSET;
+        y2[i]+=OFFSET;
+
+        for(int r=x1[i]; r<x2[i]; r++){
+            for(int c=y1[i]; c<y2[i]; c++){
+                if(i==0){
+                    rec[r][c] = 1;
+                } else {
+                    rec[r][c] = 0;
+                }
+            }
+        }
+    }
+
+    minx = x1[0];
+    miny = y1[0];
+    maxx = x2[0];
+    maxy = y2[0];
+
+    for(int i=x1[0]; i<x2[0]; i++){
+        for(int j=y1[0]; j<y1[0]; j++){
+            if((rec[i][j]==1)&&(i<minx)&&(j<miny)){
+                minx = i;
+                miny = j;
+            } else if((rec[i][j]==1)&&(i>maxx)&&(j>maxy)){
+                maxx = i;
+                maxy = j;
+            }
+        }
+    }
+
+    for(int i=minx; i<maxx; i++){
+        for(int j=miny; j<maxy; j++){
+            sum++;
+        }
+    }
+
+    cout << sum;
+    return 0;
+}
