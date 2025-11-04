25e42b22e5df286e8eb823660765f4e1c8177ed7
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-11-04 14:55:14 +0900

    Title: 거울에 레이저 쏘기 2, Time: 17ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 251104/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+bool InRange(int x, int y, int n){
+    return x>=0 && x<n && y>=0 && y<n;
+}
+
+int main() {
+    int n;
+    char arr[1000][1000] = {};
+    int dx[4] = {0, -1, 0, 1}, dy[4] = {1,0,-1,0};
+    int dir;
+    int k;
+    int q, xo;
+    int r, c;
+    int cnt = 0;
+
+    cin >> n;
+
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> arr[i][i];
+        }
+    }
+    
+    cin >> k;
+
+    k--;
+    q = k/n;
+    xo = k%n;
+    dir = q;
+
+    if(q%2==0){
+        r = q%n;
+        c = (q>=2)?(n-1-xo):xo;
+    } else {
+        r = (q>=2)?(n-1-xo):xo;
+        c = q%n;
+    }
+
+    // cout << dir << endl;
+    // cout << r << " "<< c << endl;
+
+    while(InRange(r, c, n)){
+        cnt++;
+        if(arr[r][c]=='\\'){
+            if(dir%2==0){
+                dir = (dir+3)%4;
+            } else{
+                dir = (dir+1)%4;
+            }
+        } else if(arr[r][c] == '/'){
+            if(dir%2!=0){
+                dir = (dir+3)%4;
+            } else{
+                dir = (dir+1)%4;
+            }
+        }
+
+        r += dy[dir];
+        c += dx[dir];
+        // cout << dir << endl;
+        // cout << r << " "<< c << endl;
+    }
+
+    cout << cnt;
+    return 0;
+}
