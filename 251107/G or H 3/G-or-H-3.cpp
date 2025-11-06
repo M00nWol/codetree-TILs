7be03622b4c1768cf1b53fd06f49b13d65043db5
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-11-07 08:45:19 +0900

    Title: G or H 3, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251107/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    int n, k, idx;
+    int max_idx = 0;
+    char c[100] = {};
+    int max_score=INT_MIN;
+    int score;
+
+    cin >> n >> k;
+
+    for(int i=0; i<n; i++){
+        cin >> idx;
+        cin >> c[idx];
+        max_idx = max(max_idx, idx);
+    }
+
+    for(int i=1; i<=max_idx-k; i++){
+        score = 0;
+        for(int j=i; j<=i+k; j++){
+            if(c[j]=='G'){
+                score += 1;
+            } else if(c[j]=='H'){
+                score += 2;
+            }
+        }
+        max_score = max(max_score, score);
+        
+    }
+
+    cout << max_score;
+
+    return 0;
+}
