17b51e11a86f8cde9a48db8b01d4e1d3ff952490
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-11-14 18:05:23 +0900

    Title: 스승의 은혜 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251114/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 251114/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    int n, b, cnt;
+    int n, b, cnt, tmpb;
     int cnt_max = 0;
     int p[1000] = {};
     int s[1000] = {};
@@ -20,6 +20,7 @@ int main() {
 
     // 1명씩 반값
     for(int i=0; i<n; i++){
+        tmpb = b;
         cnt = 0;
         // 임시 배열
         for(int j=0; j<n; j++){
@@ -29,11 +30,15 @@ int main() {
         tmp[i] -= (p[i]/2);
 
         sort(tmp, tmp+n);
+
+        for(int k=0; k<n; k++){
+            cout << tmp[k] << endl;
+        }
         
         int idx = 0;
 
-        while(b>0){
-            b -= tmp[idx];
+        while(tmpb>0){
+            tmpb -= tmp[idx];
             if(b<0) break;
             cnt++;
             idx++;
