c76e022c1b2e52129c91262fbf72a2fcdfe4142d
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-11-14 12:47:55 +0900

    Title: 스승의 은혜 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251114/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n, b;
+    int money;
+    int cnt;
+    int cnt_max = 0;
+    int p[1000] = {};
+
+    cin >> n >> b;
+
+    for(int i=0; i<n; i++){
+        cin >> p[i];
+    }
+
+    sort(p, p+n);
+
+    for(int i=0; i<n; i++){
+        money = b;
+        cnt = 0;
+        for(int j=0; j<n; j++){
+
+            if(i==j){
+                money -= (p[j]/2);
+            } else {
+                money -= p[j];
+            }
+            if(money<0){
+                break;
+            } else if(money==0){
+                cnt++;
+                break;
+            } else {
+                cnt++;
+            }
+        }
+
+        cnt_max = max(cnt_max, cnt);
+        cout << endl;
+    }
+
+    cout << cnt_max;
+    return 0;
+}
