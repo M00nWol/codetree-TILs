0d811a1c753ef0ea5f90b28989086fe530ced4d6
M00nWol <129468327+M00nWol@users.noreply.github.com>
2025-11-14 10:51:52 +0900

    Title: 겹치지 않는 선분 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251114/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+
+#define MAX 1000000
+using namespace std;
+
+class Line{
+    public:
+        int start;
+        int end;
+
+        Line(){};
+        Line(int start, int end){
+            this->start = start;
+            this->end = end;
+        }
+};
+
+bool Cmp(const Line& a, const Line& b){
+    return a.start < b.start;
+}
+
+int main() {
+    Line line[100];   
+    int flag[100] = {};
+    int n;
+    int a, b;
+    int cnt = 0;
+
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> line[i].start >> line[i].end;
+    }
+
+    sort(line, line+n,Cmp);
+
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(flag[i]) break;
+            if(flag[j]) continue;
+            
+            if(line[i].end > line[j].end){
+                flag[i] = 1;
+                flag[j] = 1;
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        if(flag[i]==0){
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
